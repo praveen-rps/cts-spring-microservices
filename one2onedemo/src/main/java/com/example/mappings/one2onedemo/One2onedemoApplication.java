8d20ed7c0416d0c35f93a5dcc15042c7ce1cdcc8
praveen-rps <109707042+praveen-rps@users.noreply.github.com>
2024-03-19 11:55:01 +0530

    Add files via upload

--- one2onedemo/src/main/java/com/example/mappings/one2onedemo/One2onedemoApplication.java
+++ one2onedemo/src/main/java/com/example/mappings/one2onedemo/One2onedemoApplication.java
@@ -5,19 +5,19 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.mappings.one2onedemo.dao.UserDao;
-import com.example.mappings.one2onedemo.dao.UserProfileDao;
-import com.example.mappings.one2onedemo.model.User;
-import com.example.mappings.one2onedemo.model.UserProfile;
+import com.example.mappings.one2onedemo.dao.TUserDao;
+import com.example.mappings.one2onedemo.dao.TUserProfileDao;
+import com.example.mappings.one2onedemo.model.TUserProfile;
+import com.example.mappings.one2onedemo.model.Tuser;
 
 @SpringBootApplication
 public class One2onedemoApplication implements CommandLineRunner {
 	
 	@Autowired
-	UserDao userDao;
+	TUserDao userDao;
 	
 	@Autowired
-	UserProfileDao userProfileDao;
+	TUserProfileDao userProfileDao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(One2onedemoApplication.class, args);
@@ -27,6 +27,9 @@ public class One2onedemoApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		
+		/*
+		 * Below code will implement one to one bidirectional 
+		 * 
 		UserProfile userProfile = new UserProfile();
         userProfile.setAddress("123 Main St");
         
@@ -45,7 +48,16 @@ public class One2onedemoApplication implements CommandLineRunner {
        
         System.out.println("one2one mapping done..");
 
+		*/
 		
+		// below code will implement one to one unidirectional
+		
+		TUserProfile tprofile = new TUserProfile(100001L,"user1@gmail.com");
+		Tuser tuser = new Tuser(100001L,"user1",tprofile);
+		
+		userDao.save(tuser);
+		System.out.println("one2one mapping done..");
+	
 	}
 
 }
