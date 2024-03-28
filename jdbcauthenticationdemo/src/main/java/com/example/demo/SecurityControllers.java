51021650ded99547af0a9cd8285f6d103d52aa08
praveen-rps <109707042+praveen-rps@users.noreply.github.com>
2024-03-28 10:26:58 +0530

    Add files via upload

---
+++ jdbcauthenticationdemo/src/main/java/com/example/demo/SecurityControllers.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityControllers {
+	
+	
+	@GetMapping("/testadmin")
+	public String testadmin() {
+		return "Hello, Admin you are authenticated";
+	}
+	
+	@GetMapping("/testuser")
+	public String testcustomer() {
+		return "Hello, customer you are authenticated";
+	}
+	@GetMapping("/delete")
+	public String deleteCustomer() {
+		return "Hello, customer is delete successully....";
+	}
+
+}
